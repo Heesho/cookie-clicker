457a3801599ae43814a1a4ffd239bed522eb29e7
Heesho <hisham11027@gmail.com>
2024-07-12 23:35:33 -0700

    separated clicker out from nft, must use mock nft for testing

--- contracts/Clicker.sol
+++ contracts/Clicker.sol
@@ -11,7 +11,13 @@ interface ICookie {
     function burn(address account, uint256 amount) external;
 }
 
-contract Clicker is ERC721Enumerable, Ownable {
+interface IBulla {
+    function ownerOf(uint256 tokenId) external view returns (address);
+    function exists(uint256 tokenId) external view returns (bool);
+}
+
+
+contract Clicker is Ownable {
 
     /*----------  CONSTANTS  --------------------------------------------*/
 
@@ -21,9 +27,8 @@ contract Clicker is ERC721Enumerable, Ownable {
     /*----------  STATE VARIABLES  --------------------------------------*/
 
     address immutable public cookie;
+    address immutable public bulla;
 
-    uint256 public gameCost = 10000000000000000; // 0.01 ether
-    uint256 public nextClickerId = 1;
     uint256 public maxPower = 1000000000 * PRECISION;
 
     uint256 public lvlIndex;
@@ -36,111 +41,99 @@ contract Clicker is ERC721Enumerable, Ownable {
     mapping(uint256 => uint256) public buildingId_BaseCps;      // building id => base cookies per second
     mapping(uint256 => uint256) public amount_CostMultiplier;   // building amount => cost multiplier
     
-    mapping(uint256 => string) public clickerId_Name;       // clicker id => name
-    mapping(uint256 => uint256) public clickerId_Cps;       // clicker id => cookies per second
-    mapping(uint256 => uint256) public clickerId_Last;      // clicker id => last time claimed
-    mapping(uint256 => uint256) public clickerId_Power;     // clicker id => power
+    mapping(uint256 => string) public tokenId_Name;       // token id => name
+    mapping(uint256 => uint256) public tokenId_Cps;       // token id => cookies per second
+    mapping(uint256 => uint256) public tokenId_Last;      // token id => last time claimed
+    mapping(uint256 => uint256) public tokenId_Power;     // token id => power
 
-    mapping(uint256 => mapping(uint256 => uint256)) public clickerId_buildingId_Amount; // clicker id => building id => amount
-    mapping(uint256 => mapping(uint256 => uint256)) public clickerId_buildingId_Lvl;    // clicker id => building id => level
+    mapping(uint256 => mapping(uint256 => uint256)) public tokenId_buildingId_Amount; // token id => building id => amount
+    mapping(uint256 => mapping(uint256 => uint256)) public tokenId_buildingId_Lvl;    // token id => building id => level
 
     /*----------  ERRORS ------------------------------------------------*/
 
-    error Clicker__InvalidPayment();
     error Clicker__AmountMaxed();
     error Clicker__LevelMaxed();
     error Clicker__InvalidInput();
     error Clicker__NotAuthorized();
     error Clicker__UpgradeLocked();
     error Clicker__PowerMaxed();
+    error Clicker__TokenDoesNotExist();
 
     /*----------  EVENTS ------------------------------------------------*/
 
-    event Clicker__GameCostSet(uint256 cost);
-    event Clicker__ClickerMinted(address indexed owner, uint256 indexed clickerId, uint256 cost);
-    event Clicker__ClickerUpgraded(uint256 indexed clickerId, uint256 newLevel, uint256 cost, uint256 cpc);
-    event Clicker__BuildingPurchased(uint256 indexed clickerId, uint256 buildingId, uint256 newAmount, uint256 cost, uint256 cps);
-    event Clicker__BuildingUpgraded(uint256 indexed clickerId, uint256 buildingId, uint256 newLevel, uint256 cost, uint256 cps);
-    event Clicker__Clicked(uint256 indexed clickerId, uint256 amount);
-    event Clicker__Claimed(uint256 indexed clickerId, uint256 amount);
+    event Clicker__BuildingPurchased(uint256 indexed tokenId, uint256 buildingId, uint256 newAmount, uint256 cost, uint256 cps);
+    event Clicker__BuildingUpgraded(uint256 indexed tokenId, uint256 buildingId, uint256 newLevel, uint256 cost, uint256 cps);
+    event Clicker__Claimed(uint256 indexed tokenId, uint256 amount);
     event Clicker__LvlSet(uint256 lvl, uint256 cost, uint256 unlock);
     event Clicker__BuildingSet(uint256 buildingId, uint256 baseCps, uint256 baseCost);
     event Clicker__BuildingMultiplierSet(uint256 index, uint256 multiplier);
     event Clicker__MaxPowerSet(uint256 power);
-    event Clicker__BurnedForPower(uint256 clickerId, uint256 amount);
+    event Clicker__BurnedForPower(uint256 tokenId, uint256 amount);
+
+    /*----------  MODIFIERS  --------------------------------------------*/
+
+    modifier tokenExists(uint256 tokenId) {
+        if (!IBulla(bulla).exists(tokenId)) revert Clicker__TokenDoesNotExist();
+        _;
+    }
 
     /*----------  FUNCTIONS  --------------------------------------------*/
 
-    constructor(address _cookie) ERC721("Clicker", "CLICKER") {
+    constructor(address _cookie, address _bulla) {
         cookie = _cookie;
+        bulla = _bulla;
     }
 
-    function mint() external payable {
-        if (msg.value != gameCost) revert Clicker__InvalidPayment();
-        payable(owner()).transfer(gameCost);
-        _safeMint(msg.sender, nextClickerId);
-        string memory tokenIdStr = Strings.toString(nextClickerId);
-        clickerId_Name[nextClickerId] = string(abi.encodePacked("Bakery ", tokenIdStr));
-        clickerId_Last[nextClickerId] = block.timestamp;
-        emit Clicker__ClickerMinted(msg.sender, nextClickerId, gameCost);
-        nextClickerId++;
-    }
-
-    function burnForPower(uint256 clickerId, uint256 amount) external {
+    function burnForPower(uint256 tokenId, uint256 amount) external tokenExists(tokenId) {
         if (amount == 0) revert Clicker__InvalidInput();
-        claim(clickerId);
-        clickerId_Power[clickerId] += amount;
-        if (clickerId_Power[clickerId] > maxPower) revert Clicker__PowerMaxed();
+        claim(tokenId);
+        tokenId_Power[tokenId] += amount;
+        if (tokenId_Power[tokenId] > maxPower) revert Clicker__PowerMaxed();
         ICookie(cookie).burn(msg.sender, amount);
-        emit Clicker__BurnedForPower(clickerId, amount);
+        emit Clicker__BurnedForPower(tokenId, amount);
     }
 
-    function claim(uint256 clickerId) public {
-        uint256 amount = clickerId_Cps[clickerId] * (block.timestamp - clickerId_Last[clickerId]);
-        uint256 maxAmount = clickerId_Cps[clickerId] * DURATION;
+    function claim(uint256 tokenId) public tokenExists(tokenId) {
+        uint256 amount = tokenId_Cps[tokenId] * (block.timestamp - tokenId_Last[tokenId]);
+        uint256 maxAmount = tokenId_Cps[tokenId] * DURATION;
         if (amount > maxAmount) amount = maxAmount;
-        clickerId_Last[clickerId] = block.timestamp;
-        emit Clicker__Claimed(clickerId, amount);
-        ICookie(cookie).mint(ownerOf(clickerId), amount);
+        tokenId_Last[tokenId] = block.timestamp;
+        emit Clicker__Claimed(tokenId, amount);
+        ICookie(cookie).mint(IBulla(bulla).ownerOf(tokenId), amount);
     }
 
-    function purchaseBuilding(uint256 clickerId, uint256 buildingId, uint256 buildingAmount) external {
+    function purchaseBuilding(uint256 tokenId, uint256 buildingId, uint256 buildingAmount) external tokenExists(tokenId) {
         if (buildingAmount == 0) revert Clicker__InvalidInput();
-        claim(clickerId);
+        claim(tokenId);
         for (uint256 i = 0; i < buildingAmount; i++) {
-            uint256 currentAmount = clickerId_buildingId_Amount[clickerId][buildingId];
+            uint256 currentAmount = tokenId_buildingId_Amount[tokenId][buildingId];
             if (currentAmount == amountIndex) revert Clicker__AmountMaxed();
             uint256 cost = getBuildingCost(buildingId, currentAmount);
-            clickerId_buildingId_Amount[clickerId][buildingId]++;
-            clickerId_Cps[clickerId] += getBuildingCps(buildingId, clickerId_buildingId_Lvl[clickerId][buildingId]);
-            emit Clicker__BuildingPurchased(clickerId, buildingId, clickerId_buildingId_Amount[clickerId][buildingId], cost, clickerId_Cps[clickerId]);
+            tokenId_buildingId_Amount[tokenId][buildingId]++;
+            tokenId_Cps[tokenId] += getBuildingCps(buildingId, tokenId_buildingId_Lvl[tokenId][buildingId]);
+            emit Clicker__BuildingPurchased(tokenId, buildingId, tokenId_buildingId_Amount[tokenId][buildingId], cost, tokenId_Cps[tokenId]);
             ICookie(cookie).burn(msg.sender, cost);
         }
     }
 
-    function upgradeBuilding(uint256 clickerId, uint256 buildingId) external {
-        uint256 currentLvl = clickerId_buildingId_Lvl[clickerId][buildingId];
+    function upgradeBuilding(uint256 tokenId, uint256 buildingId) external tokenExists(tokenId) {
+        uint256 currentLvl = tokenId_buildingId_Lvl[tokenId][buildingId];
         uint256 cost = buildingId_BaseCost[buildingId] * lvl_CostMultiplier[currentLvl + 1];
         if (cost == 0) revert Clicker__LevelMaxed();
-        if (clickerId_buildingId_Amount[clickerId][buildingId] < lvl_Unlock[currentLvl + 1]) revert Clicker__UpgradeLocked();
-        claim(clickerId); 
-        clickerId_buildingId_Lvl[clickerId][buildingId]++;
-        clickerId_Cps[clickerId] += (getBuildingCps(buildingId, currentLvl + 1) - getBuildingCps(buildingId, currentLvl)) * clickerId_buildingId_Amount[clickerId][buildingId];
-        emit Clicker__BuildingUpgraded(clickerId, buildingId, clickerId_buildingId_Lvl[clickerId][buildingId], cost, clickerId_Cps[clickerId]);
+        if (tokenId_buildingId_Amount[tokenId][buildingId] < lvl_Unlock[currentLvl + 1]) revert Clicker__UpgradeLocked();
+        claim(tokenId); 
+        tokenId_buildingId_Lvl[tokenId][buildingId]++;
+        tokenId_Cps[tokenId] += (getBuildingCps(buildingId, currentLvl + 1) - getBuildingCps(buildingId, currentLvl)) * tokenId_buildingId_Amount[tokenId][buildingId];
+        emit Clicker__BuildingUpgraded(tokenId, buildingId, tokenId_buildingId_Lvl[tokenId][buildingId], cost, tokenId_Cps[tokenId]);
         ICookie(cookie).burn(msg.sender, cost);
     }
 
-    function setName(uint256 clickerId, string calldata name) external {
-        if (msg.sender != ownerOf(clickerId)) revert Clicker__NotAuthorized();
-        clickerId_Name[clickerId] = name;
+    function setName(uint256 tokenId, string calldata name) external tokenExists(tokenId) {
+        if (msg.sender != IBulla(bulla).ownerOf(tokenId)) revert Clicker__NotAuthorized();
+        tokenId_Name[tokenId] = name;
     }
 
     /*----------  RESTRICTED FUNCTIONS  ---------------------------------*/
-
-    function setGameCost(uint256 cost) external onlyOwner {
-        gameCost = cost;
-        emit Clicker__GameCostSet(cost);
-    }
 
     function setLvl(uint256[] calldata cost, uint256[] calldata unlock) external onlyOwner {
         if (cost.length != unlock.length) revert Clicker__InvalidInput();
